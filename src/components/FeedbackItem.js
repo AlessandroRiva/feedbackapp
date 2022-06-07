635e1afd476a3cd9164ea4bf41e7d1afa2c9ed4a
AlessandroRiva <alessandrovittorioriva@gmail.com>
2022-06-06 23:21:48 -0700

    Added delete function

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,9 +1,13 @@
+import { FaTimes } from "react-icons/fa";
 import Card from "./shared/Card";
 
-function Feedbackitem({ item }) {
+function Feedbackitem({ item, handleDelete }) {
   return (
-    <Card>
+    <Card reverse={false}>
       <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   );
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,13 +1,13 @@
 import Feedbackitem from "./FeedbackItem";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <Feedbackitem key={item.id} item={item} />
+        <Feedbackitem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
